c8fe7d198f5d5224be90c5402a569d666b0333e6
visual_community@outlook.com <visual_community@outlook.com>
2019-12-27 12:43:59 -0400

    push

---
+++ API_PHONE/API_PHONE/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API_PHONE.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ API_PHONE/API_PHONE/Models/api_phone.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_PHONE.Models
+{
+    public class api_phone
+    {
+        [Key]
+        public int PersonID { get; set; }
+
+        [Required]
+        public string First_Name { get; set; }
+
+        [Required]
+        public string Last_Name { get; set; }
+
+        [Required]
+        public BloodType Type_B { get; set; }
+
+
+    }
+
+    public enum BloodType
+    {
+        A_Pos,
+        A_Neg,
+        B_Pos,
+        B_Neg,
+        AB,
+        O,
+
+    }
+}
